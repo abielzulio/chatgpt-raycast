cc15807e28770a240b7410746fbc0b050f5ebfe0
Abiel Zulio M <abielzm@gmail.com>
2022-12-15 17:32:35 +0700

    feat: enable detailed view option

--- src/views/answer-detail.tsx
+++ src/views/answer-detail.tsx
@@ -1,18 +1,24 @@
 import { Answer } from "../type";
-import { List } from "@raycast/api";
+import { getPreferenceValues, List } from "@raycast/api";
+import { useState } from "react";
 
 export const AnswerDetailView = (props: { answer: Answer; markdown?: string | null | undefined }) => {
   const { answer, markdown } = props;
+  const [isDetailedView] = useState(() => {
+    return getPreferenceValues<{ isDetailedView: boolean }>().isDetailedView;
+  });
   return (
     <List.Item.Detail
       markdown={markdown ?? `**${answer.question}**\n\n${answer.answer}`}
       metadata={
-        <List.Item.Detail.Metadata>
-          <List.Item.Detail.Metadata.Label title="Date" text={new Date(answer.createdAt ?? 0).toLocaleString()} />
-          <List.Item.Detail.Metadata.Separator />
-          <List.Item.Detail.Metadata.Label title="ID" text={answer.id} />
-          <List.Item.Detail.Metadata.Label title="Conversation ID" text={answer.conversationId} />
-        </List.Item.Detail.Metadata>
+        isDetailedView ? (
+          <List.Item.Detail.Metadata>
+            <List.Item.Detail.Metadata.Label title="Date" text={new Date(answer.createdAt ?? 0).toLocaleString()} />
+            <List.Item.Detail.Metadata.Separator />
+            <List.Item.Detail.Metadata.Label title="ID" text={answer.id} />
+            <List.Item.Detail.Metadata.Label title="Conversation ID" text={answer.conversationId} />
+          </List.Item.Detail.Metadata>
+        ) : undefined
       }
     />
   );
